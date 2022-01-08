fe96927fb85b51ded3fec0d07a3acb0c196ce766
Abdalla Walled <www.abdallawalled06@gmail.com>
2022-01-08 04:47:28 -0800

    Finish personal account update #38

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:najme/screens/primary_screens/login_screen.dart';
-import 'package:najme/screens/primary_screens/personal_account_updates.dart';
 
 
 class NajmeApp extends StatelessWidget {
@@ -9,7 +8,7 @@ class NajmeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: PersonalAccountUpdates(),
+      home: LoginScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
