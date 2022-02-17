3cd91a6cf7c149972c475d67531050e16045d91a
Mostafa Amin <mostafa.amin.gaber@gmail.com>
2022-02-17 18:37:10 +0200

    Add svg and png icons from UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:najme/screens/drawer/personal_profile_updates.dart';
 import 'package:najme/screens/main/launch_screen.dart';
 import 'package:najme/database/init.dart';
 
@@ -13,7 +14,7 @@ class NajmeApp extends StatelessWidget {
     database.init();
 
     return  MaterialApp(//const
-      home: LaunchScreen(),
+      home: PersonalProfileUpdates(), //LaunchScreen
       debugShowCheckedModeBanner: false,
     );
   }
