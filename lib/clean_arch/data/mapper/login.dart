bc3131b5bd6cdb50df6b25b9f3100b8ceeaa36b8
ManarShahin48 <manarshahin48@gmail.com>
2023-01-18 23:11:31 +0200

    Adding mappers to make sure that models will not be null

---
+++ lib/clean_arch/data/mapper/login.dart
@@ -0,0 +1,29 @@
+import 'package:najme/clean_arch/app/constant.dart';
+import 'package:najme/clean_arch/data/response/login/login.dart';
+import 'package:najme/clean_arch/domain/models/login.dart';
+import 'package:najme/clean_arch/app/extensions.dart';
+
+// To make sure that models will not be null
+extension CustomerResponseMapper on CustomerResponse? {
+  Customer toDomain() {
+    return Customer(
+      this?.id.orEmpty() ?? Constants.empty,
+      this?.name.orEmpty() ?? Constants.empty,
+    );
+  }
+}
+
+extension ContactsResponseMapper on ContactsResponse? {
+  Contacts toDomain() {
+    return Contacts(
+        this?.phone.orEmpty() ?? Constants.empty,
+        this?.email.orEmpty() ?? Constants.empty
+    );
+  }
+}
+
+extension AuthenticationResponseMapper on AuthenticationResponse? {
+  Authentication toDomain() {
+    return Authentication(this?.customer.toDomain(), this?.contacts.toDomain());
+  }
+}
