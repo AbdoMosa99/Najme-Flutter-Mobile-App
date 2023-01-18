7be860b25fe213fbcdf1f37d1268989a2e56e979
ManarShahin48 <manarshahin48@gmail.com>
2023-01-18 17:59:57 +0200

    Adding login model

---
+++ lib/clean_arch/domain/models/login.dart
@@ -0,0 +1,22 @@
+class Customer {
+  String id;
+  String name;
+  int numOfNotifications;
+
+  Customer(this.id, this.name, this.numOfNotifications);
+}
+
+class Contacts {
+  String phone;
+  String email;
+  String link;
+
+  Contacts(this.phone, this.email, this.link);
+}
+
+class Authentication {
+  Customer? customer;
+  Contacts? contacts;
+
+  Authentication(this.customer, this.contacts);
+}
